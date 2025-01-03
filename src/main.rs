a38209f7eb3090533ee1cb9e15db1dc2573b0ec9
jwt2706 <jwt2home@gmail.com>
2025-01-03 15:48:42 -0500

    VGA HELLO WORLD

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,19 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static BOOT_MSG: &[u8] = b"BabsocOS booted up successfully!";
+
 // this is the entry point, for now
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+    for (i, &character) in BOOT_MSG.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = character; // character to print
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xF; // color
+        }
+    }
+    
+    
     loop {}
 }
